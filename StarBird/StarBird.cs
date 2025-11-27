9e10b6c4cfab100457e025adbc0a9997a13a3fe7
Gorgol <marcincwalina00123456@gmail.com>
2025-11-27 20:02:21 +0100

    Add scanner implementation

---
+++ StarBird/StarBird.cs
@@ -0,0 +1,71 @@
+﻿namespace StarBird;
+
+public class StarBird
+{
+    static bool hadError = false;
+    public static void Main(string[] args)
+    {
+        if (args.Length > 1)
+        {
+            Console.WriteLine("Usage: StarBird [script]");
+            Environment.Exit(64);
+        }
+        else if (args.Length == 1)
+        {
+            runFile(args[0]);
+        }
+        else
+        {
+            runPrompt();
+        }
+    }
+    
+    private static void runPrompt()
+    {
+        hadError = false;
+
+        while (true)
+        {
+            Console.Write("> ");
+            string line = Console.ReadLine();
+
+            if (line == null)
+                break;
+
+            run(line);
+            hadError = false;
+        }
+    }
+    
+    private static void runFile(string path)
+    {
+        string source = File.ReadAllText(path);
+        run(source);
+
+        if (hadError) Environment.Exit(65);
+    }
+    
+    private static void run(string source)
+    {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        foreach (var token in tokens)
+        {
+            Console.WriteLine(token);
+        }
+    }
+
+    
+    public static void error(int line, string message)
+    {
+        report(line, "", message);
+    }
+
+    private static void report(int line, string where, string message)
+    {
+        Console.Error.WriteLine($"[line {line}] Error{where}: {message}");
+        hadError = true;
+    }
+}
+
---
+++ StarBird/scanner.cs
@@ -0,0 +1,205 @@
+namespace StarBird;
+
+public class Scanner {
+    private readonly String source;
+    private readonly List<Token> tokens = new List<Token>();
+    
+    private int start;
+    private int current;
+    private int line = 1;
+    
+    private static readonly Dictionary<string, TokenType> keywords = new()
+    {
+        { "and",    TokenType.AND },
+        { "class",  TokenType.CLASS },
+        { "else",   TokenType.ELSE },
+        { "false",  TokenType.FALSE },
+        { "for",    TokenType.FOR },
+        { "fun",    TokenType.FUN },
+        { "if",     TokenType.IF },
+        { "nil",    TokenType.NIL },
+        { "or",     TokenType.OR },
+        { "print",  TokenType.PRINT },
+        { "return", TokenType.RETURN },
+        { "super",  TokenType.SUPER },
+        { "this",   TokenType.THIS },
+        { "true",   TokenType.TRUE },
+        { "var",    TokenType.VAR },
+        { "while",  TokenType.WHILE }
+    };
+    
+    public Scanner(String source) {
+        this.source = source;
+    }
+    private bool IsAtEnd()
+    {
+        return current >= source.Length;
+    }
+
+    public List<Token> scanTokens() {
+        while (!IsAtEnd()) {
+            start = current;
+            scanToken();
+        }
+
+        tokens.Add(new Token(TokenType.EOF, "", null, line));
+        return tokens;
+    }
+    
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(': addToken(TokenType.LEFT_PAREN); break;
+            case ')': addToken(TokenType.RIGHT_PAREN); break;
+            case '{': addToken(TokenType.LEFT_BRACE); break;
+            case '}': addToken(TokenType.RIGHT_BRACE); break;
+            case ',': addToken(TokenType.COMMA); break;
+            case '.': addToken(TokenType.DOT); break;
+            case '-': addToken(TokenType.MINUS); break;
+            case '+': addToken(TokenType.PLUS); break;
+            case ';': addToken(TokenType.SEMICOLON); break;
+            case '*': addToken(TokenType.STAR); break; 
+            case '!':
+                addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+                break;
+            case '=':
+                addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+                break;
+            case '>':
+                addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
+                break;
+            case '/':
+                if (match('/')) {
+                    while (peek() != '\n' && !IsAtEnd()) advance();
+                } else {
+                    addToken(TokenType.SLASH);
+                }
+                break;
+            case ' ':
+            case '\r':
+            case '\t':
+                break;
+
+            case '\n':
+                line++;
+                break;
+            case '"': String(); break;
+            case 'o':
+                if (match('r')) {
+                    addToken(TokenType.OR);
+                }
+                else if (isAlpha(c))
+                {
+                    identifier();
+                }
+                else
+                {
+                    StarBird.error(line, "Unexpected character.");
+                }
+                break;
+
+            default:
+                if (isDigit(c)) {
+                    number();
+                } else {
+                    StarBird.error(line, "Unexpected character.");
+                }
+                break;
+        }
+    }
+    private bool match(char expected) 
+    {
+        if (IsAtEnd()) return false;
+        if (source[current] != expected) return false;
+
+        current++;
+        return true;
+    }
+    
+    private char advance() 
+    {
+        return source[current++];
+    }
+
+    private void addToken(TokenType type) 
+    {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) 
+    {
+        String text = source.Substring(start, current);
+        tokens.Add(new Token(type, text, literal, line));
+    }
+    
+    private void String() 
+    {
+        while (peek() != '"' && !IsAtEnd()) {
+            if (peek() == '\n') line++;
+            advance();
+        }
+
+        if (IsAtEnd()) {
+            StarBird.error(line, "Unterminated string.");
+            return;
+        }
+        
+        advance();
+        
+        String value = source.Substring(start + 1, current - 1);
+        addToken(TokenType.STRING, value);
+    }
+    
+    private bool isDigit(char c) {
+        return c >= '0' && c <= '9';
+    } 
+    
+    private void number() {
+        while (isDigit(peek())) advance();
+        
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance();
+
+            while (isDigit(peek())) advance();
+        }
+
+        addToken(TokenType.NUMBER,
+            Double.Parse(source.Substring(start, current)));
+    }
+    
+    private char peekNext() {
+        if (current + 1 >= source.Length) return '\0';
+        return source[current + 1];
+    } 
+    
+    private char peek() 
+    {
+        if (IsAtEnd()) return '\0';
+        return source[current];
+    }
+    
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+        String text = source.Substring(start, current);
+        TokenType type;
+        if (!keywords.TryGetValue(text, out type))
+        {
+            type = TokenType.IDENTIFIER;
+        }
+
+        addToken(type);
+    }
+    
+    private bool isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+               (c >= 'A' && c <= 'Z') ||
+               c == '_';
+    }
+
+    private bool isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+}   
